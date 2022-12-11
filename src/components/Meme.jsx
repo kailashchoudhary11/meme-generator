e3151363b515f49eb1706c45eebe0e1f41af17dd
kai <kailashjat604@gmail.com>
2022-12-11 16:31:07 +0530

    Create Form

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import "./Meme.css";
+import memesData from "../memesData";
 
 function Meme() {
+    const [imageUrl, setImageUrl] = React.useState();
+    function getMemeImage() {
+        const memes = memesData.data.memes;
+        const randomMeme = memes[Math.floor(Math.random() * memes.length)];
+        setImageUrl(url => randomMeme.url);
+    }
     return (
         <main>
-            <form className="form"action="">
+            <div className="form">
                 <input 
                 className="form--input" type="text" placeholder="Top Text"
                 />
                 <input 
                 className="form--input" type="text" placeholder="Bottom Text"
                 />
-                <button className="form--button">
+                <button className="form--button" onClick={getMemeImage}>
                     Get a new meme image  🖼
                 </button>
-            </form>
+            </div>
+            {imageUrl && 
+                <img 
+                    src={imageUrl} 
+                    alt="Random Meme" 
+                    className="meme--img"
+                />
+            }
         </main>
     );
 }
